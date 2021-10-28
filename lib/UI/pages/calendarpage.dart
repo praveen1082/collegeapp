ad77b823f72e3396553296092d6970e9b0a6b2b0
@praveen1082 <Praveenpanta1082@gmail.com>
2021-10-28 10:42:07 +0545

    Working on calendar page

--- lib/UI/pages/calendarpage.dart
+++ lib/UI/pages/calendarpage.dart
@@ -1,78 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../../utils.dart';
 
 class CalendarPage extends StatefulWidget {
-  const CalendarPage({Key? key}) : super(key: key);
-
   @override
   _CalendarPageState createState() => _CalendarPageState();
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Academic Calendar"),
-        backgroundColor: Colors.lightBlue[900],
+        title: Text('Academic Calendar'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.chevron_left_outlined,
-                  size: 40.0,
-                ),
-                Expanded(
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "October 2021",
-                          style: TextStyle(
-                              fontSize: 20.0, fontWeight: FontWeight.w500),
-                        ))),
-                Icon(
-                  Icons.chevron_right_outlined,
-                  size: 40.0,
-                ),
-              ],
+      body: TableCalendar(
+        startingDayOfWeek: StartingDayOfWeek.sunday,
+        headerStyle: HeaderStyle(
+            formatButtonShowsNext: false,
+            formatButtonVisible: false,
+            titleCentered: true),
+        weekendDays: [5],
+        firstDay: kFirstDay,
+        lastDay: kLastDay,
+        calendarStyle: CalendarStyle(
+            selectedDecoration: BoxDecoration(
+              color: Colors.blue,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Table(
-              border: TableBorder.all(
-                  color: Colors.black, style: BorderStyle.solid, width: 1),
-              children: [
-                TableRow(children: [
-                  Column(children: [
-                    Text('Sun', style: TextStyle(fontSize: 14.0))
-                  ]),
-                  Column(children: [
-                    Text('Mon', style: TextStyle(fontSize: 14.0))
-                  ]),
-                  Column(children: [
-                    Text('Tues', style: TextStyle(fontSize: 14.0))
-                  ]),
-                  Column(children: [
-                    Text('Wed', style: TextStyle(fontSize: 14.0))
-                  ]),
-                  Column(children: [
-                    Text('Thurs', style: TextStyle(fontSize: 14.0))
-                  ]),
-                  Column(children: [
-                    Text('Fri', style: TextStyle(fontSize: 14.0))
-                  ]),
-                  Column(children: [
-                    Text('Sat', style: TextStyle(fontSize: 14.0))
-                  ]),
-                ]),
-              ],
-            ),
-          )
-        ],
+            todayDecoration: BoxDecoration(
+              color: Colors.green[400],
+            )),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
       ),
     );
   }
