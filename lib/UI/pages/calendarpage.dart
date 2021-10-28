f2624377c9b71f059e7df07b02e4b4b979419e70
@praveen1082 <Praveenpanta1082@gmail.com>
2021-10-28 11:13:22 +0545

    Calendar work completed remaining to insert button in bottom of page

--- lib/UI/pages/calendarpage.dart
+++ lib/UI/pages/calendarpage.dart
@@ -19,53 +19,186 @@ class _CalendarPageState extends State<CalendarPage> {
       appBar: AppBar(
         title: Text('Academic Calendar'),
       ),
-      body: TableCalendar(
-        startingDayOfWeek: StartingDayOfWeek.sunday,
-        headerStyle: HeaderStyle(
-            formatButtonShowsNext: false,
-            formatButtonVisible: false,
-            titleCentered: true),
-        weekendDays: [5],
-        firstDay: kFirstDay,
-        lastDay: kLastDay,
-        calendarStyle: CalendarStyle(
-            selectedDecoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            todayDecoration: BoxDecoration(
-              color: Colors.green[400],
-            )),
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        selectedDayPredicate: (day) {
-          // Use `selectedDayPredicate` to determine which day is currently selected.
-          // If this returns true, then `day` will be marked as selected.
+      body: ListView(
+        children: [
+          TableCalendar(
+            startingDayOfWeek: StartingDayOfWeek.sunday,
+            headerStyle: HeaderStyle(
+                formatButtonShowsNext: false,
+                formatButtonVisible: false,
+                titleCentered: true),
+            weekendDays: [6],
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            calendarStyle: CalendarStyle(
+                // weekendTextStyle: TextStyle(color: Colors.red),
+                selectedDecoration: BoxDecoration(color: Colors.blueGrey),
+                todayDecoration: BoxDecoration(
+                  color: Colors.green[400],
+                )),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              // Use `selectedDayPredicate` to determine which day is currently selected.
+              // If this returns true, then `day` will be marked as selected.
 
-          // Using `isSameDay` is recommended to disregard
-          // the time-part of compared DateTime objects.
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            // Call `setState()` when updating the selected day
-            setState(() {
-              _selectedDay = selectedDay;
+              // Using `isSameDay` is recommended to disregard
+              // the time-part of compared DateTime objects.
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                // Call `setState()` when updating the selected day
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                // Call `setState()` when updating calendar format
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              // No need to call `setState()` here
               _focusedDay = focusedDay;
-            });
-          }
-        },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            // Call `setState()` when updating calendar format
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          // No need to call `setState()` here
-          _focusedDay = focusedDay;
-        },
+            },
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            height: 20, width: 25, color: Colors.green[400]),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Text("Today")
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(height: 20, width: 25, color: Colors.red),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Text("Holiday")
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(height: 20, width: 25, color: Colors.amber),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Text("Exam")
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            height: 20, width: 25, color: Colors.blueAccent),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Text("Fee Submit Date")
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            height: 20, width: 25, color: Colors.green[800]),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Text("Class Start Date")
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            height: 20, width: 25, color: Colors.purpleAccent),
+                        SizedBox(
+                          width: 15.0,
+                        ),
+                        Text("Other Events")
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
